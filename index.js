b334fe1bc20dabdf71130d2ac7aab2d87e2790d0
elbfn103 <liu.eric103@gmail.com>
2021-05-04 13:25:57 -0700

    added basic chat functionality, shows user connections and disconnections

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    io.emit('user connect');
+    socket.on('disconnect', () => {
+        io.emit('user disconnect');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 server.listen(3000, () => {
